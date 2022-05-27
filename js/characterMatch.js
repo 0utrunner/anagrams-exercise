9fd3587b0f4e5c000a832f8b4241ecdcfacafc9a
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-26 23:02:40 -0500

    Javascript Solved

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,19 @@
 exports.isCharacterMatch = function(string1, string2) {
-
+    string1 = string1.toLowerCase().split(' ').join('').split('')
+    string2 = string2.toLowerCase().split(' ').join('').split('')
+    let counter = string1.length + string2.length
+    while(counter > 0){
+        if(string1[0] == string2[0]){
+            string1.shift()
+            string2.shift()
+        } else {
+            string2.push(string2.shift())
+            counter--
+        }
+        if(string1.length == 0){
+            break
+        }
+    }
+    return string1.length == 0 ? true : false
 };
+
